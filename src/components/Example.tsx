0ff71efb283acb23d682ee28addcf970a2fca147
Sergei Sivtsev <sergykt@gmail.com>
2024-06-24 22:39:36 +0500

    chore: add styled components

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,6 +1,15 @@
 import { type FC } from 'react';
+import styled from 'styled-components';
 import styles from './Example.module.scss';
 
-const Example: FC = () => <div className={styles.box}>Example</div>;
+const Button = styled.button`
+  color: red;
+`;
+
+const Example: FC = () => (
+  <div className={styles.box}>
+    <Button className='button'>Example</Button>
+  </div>
+);
 
 export default Example;
