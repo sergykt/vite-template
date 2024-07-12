975fa359912e5f86eca21c63cd9d8c0ad8050f3d
Sergei Sivtsev <sergykt@gmail.com>
2024-07-12 19:54:22 +0500

    test pre-push

--- src/components/Example.test.ts
+++ src/components/Example.test.ts
@@ -2,6 +2,6 @@ import { expect, test } from 'vitest';
 
 const sum = (a: number, b: number) => a + b;
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5);
 });
